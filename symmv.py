e70ca6c8048b12d06d9b56e47a037ca4fe73b835
Rob Lanphier <robla@robla.net>
2017-05-19 16:32:51 -0700

    symmv.py: PEP8 cleanup

--- symmv.py
+++ symmv.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import argparse
 
+
 def symmv(src, dst):
     src = src.rstrip('/')
     if os.path.islink(src):
@@ -16,16 +17,19 @@ def symmv(src, dst):
         else:
             os.symlink(dst, src)
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description='mv a file from src to dst, then symlink src to dst' ) 
+        description='mv a file from src to dst, then symlink src to dst')
     parser.add_argument('src', help='original file')
     parser.add_argument('dst', help='place to move')
     return parser.parse_args()
 
+
 def main():
     args = parse_arguments()
     symmv(args.src, args.dst)
+
 
 if __name__ == "__main__":
     main()
